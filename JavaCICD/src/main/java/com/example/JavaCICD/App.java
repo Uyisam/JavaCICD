3839afea77022bbe2ed7a3c9e8a6580b1ffb4f1a
üyisam <uyisamaboobakkar@gmail.com>
2026-03-04 01:25:15 +0530

    Inital Commit

---
+++ JavaCICD/src/main/java/com/example/JavaCICD/App.java
@@ -0,0 +1,10 @@
+package com.example.JavaCICD;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ JavaCICD/src/main/java/com/example/JavaCICD/Main.java
@@ -0,0 +1,9 @@
+package com.example.JavaCICD;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello CICD");
+	}
+
+}
